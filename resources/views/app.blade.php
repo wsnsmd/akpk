ad358241d5e5411cb08d8a54b618170ae733467b
wsnsmd <wsnsmd@gmail.com>
2022-03-07 14:26:42 +0800

    first commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="Analisis Kebutuhan Pengembangan Kompetensi">
+    <meta name="keywords" content="Analisis,Kebutuhan,Pengembangan,Kompetensi,BPSDM">
+    <meta name="author" content="IT BPSDM Prov. Kaltim">
+
+    <!-- Icons -->
+    <link rel="shortcut icon" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('android-icon-192x192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-icon-precomposed.png') }}">
+
     <link href="https://rsms.me/inter/inter.css" rel="stylesheet">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('js/manifest.js') }}" defer></script>
