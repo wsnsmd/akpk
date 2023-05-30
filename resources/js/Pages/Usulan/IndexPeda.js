c8239cf0e1313d590f097da84da6baa468574a00
wsnsmd <wsnsmd@gmail.com>
2023-05-31 00:34:01 +0800

    update rakernis 2023 s7

--- resources/js/Pages/Usulan/IndexPeda.js
+++ resources/js/Pages/Usulan/IndexPeda.js
@@ -37,7 +37,7 @@ const Index = () => {
     {
       name: 'Verifikasi',
       cell: (row) => 
-        row.is_verify
+        (row.is_verify == 1)
           ? <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
               Sudah
             </span>
